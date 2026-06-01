0a9cd1a4d35bc75d165c8663d50a7f170f6c802c
Christian Storch <ccstorch@gmail.com>
2026-06-01 10:01:46 +0200

    feat(flex): add alignSelf modifier shortcuts
    selfStretch, selfCenter, selfStart, selfEnd as boolean props

--- src/modifiers/flex.js
+++ src/modifiers/flex.js
@@ -1,11 +1,16 @@
 import { clearProps, flattenStyle } from './_helpers'
 
 export function useFlexModifier([values, props]) {
-  let { flex, ...restProps } = props
+  let { flex, alignSelf, selfStretch, selfCenter, selfStart, selfEnd, ...restProps } = props
 
   if (flex === true) flex = 1
 
-  const style = clearProps({ flex, minWidth: 0 })
+  if (selfStretch) alignSelf = 'stretch'
+  if (selfCenter) alignSelf = 'center'
+  if (selfStart) alignSelf = 'flex-start'
+  if (selfEnd) alignSelf = 'flex-end'
+
+  const style = clearProps({ flex, minWidth: 0, alignSelf })
 
   return [
     values,
